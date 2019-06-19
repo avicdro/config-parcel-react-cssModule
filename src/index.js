5b4fcf06c2aa5cf85d738ba5411117531870f3a9
avicdro <avicdro@gmail.com>
2019-06-19 16:14:16 +0200

    Reorganizar las carpetas y normalize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import classes from './index.module.css'
 
+import "normalize.css"
+import styled from './index.module.css'
 // Components 
+import App from './App.js'
 import ContenedorDeObjetos from './components/ContenedorDeObjetos/ContenedorObjetos'
 
 export default class Index extends Component {
   render() {
     return (
-      <div className={classes.content}>
+      <div className={styled.content}>
+        <App />
         <ContenedorDeObjetos />
       </div>
     )
